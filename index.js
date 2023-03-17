a7af4df6d686211eb84695369850519e98234ccd
Roberto Marungo <robmarungo.dev@gmail.com>
2023-03-17 01:40:47 -0500

    Completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
+const num1 = 2;
 
-// Write your code here
+const num2 = 31;
+
+let multiply = num1 * num2;
+
+let random = Math.floor(Math.random() * 5) + 1;
+
+const num3 = 24;
+
+const num4 = 5;
+
+let mod = num3 % num4;
+
+let max = Math.max(5, 10, 15, 20);
